3114ac1f7f8291970a9614093eba0eaf9496d335
Antonio Mistretta <ymsimei@gmail.com>
2019-01-11 02:51:02 -0500

    added bootstrap

---
+++ _site/scripts/typewriter.js
@@ -0,0 +1,5 @@
+const instance = new Typewriter('#word', {
+  strings: ["engaging", "unifying", "interactive", "civil", "unbiased", "welcoming", "uniting", "accessible", "unprejudiced", "representative", "inclusive", "understandable", "relatable", "cordial", "hospitable", "diverse", "different"],
+  autoStart: true,
+  loop: true
+});
