5d4939cdfff5c928df60a9190f9791c412c28708
Antonio Mistretta <ymsimei@gmail.com>
2019-04-27 19:02:36 -0400

    added burger

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,19 +1,41 @@
-addRemoveOpaqueClass()
+// addRemoveOpaqueClass()
+//
+// $(window).scroll(function () {
+//   addRemoveOpaqueClass()
+// });
+//
+// function checkScroll() {
+//   let scrollPosition = $(this).scrollTop();
+//   return scrollPosition;
+// }
+//
+// function addRemoveOpaqueClass() {
+//   if (checkScroll() > 20) {
+//    $('nav').addClass('opaque');
+//   }
+//   if (checkScroll() < 20) {
+//    $('nav').removeClass('opaque');
+//   }
+// }
 
-$(window).scroll(function () {
-  addRemoveOpaqueClass()
-});
+let navbar = $('.nav')
 
-function checkScroll() {
-  let scrollPosition = $(this).scrollTop();
-  return scrollPosition;
-}
+// Check Window Scroll Postion
+$(window).scroll(function() {
+    var height = $(window).scrollTop()
+    if (height > 10) {
+      navbar.addClass('short')
+    } else {
+      navbar.removeClass('short')
+    }
+})
 
-function addRemoveOpaqueClass() {
-  if (checkScroll() > 20) {
-   $('nav').addClass('opaque');
+let extended = false
+function extendMenu() {
+  if (extended) {
+    document.querySelector(".nav").classList.remove("extended")
+  } else {
+    document.querySelector(".nav").classList.add("extended")
   }
-  if (checkScroll() < 20) {
-   $('nav').removeClass('opaque');
-  }
+  extended = !extended
 }
