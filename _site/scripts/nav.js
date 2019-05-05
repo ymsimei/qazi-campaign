9e3cb3fdb5a2041029a326e92dcea2145e926bcf
Antonio Mistretta <ymsimei@gmail.com>
2019-05-05 17:43:54 -0400

    updated page

--- _site/scripts/nav.js
+++ _site/scripts/nav.js
@@ -1,36 +1,14 @@
-// addRemoveOpaqueClass()
-//
-// $(window).scroll(function () {
-//   addRemoveOpaqueClass()
-// });
-//
-// function checkScroll() {
-//   let scrollPosition = $(this).scrollTop();
-//   return scrollPosition;
-// }
-//
-// function addRemoveOpaqueClass() {
-//   if (checkScroll() > 20) {
-//    $('nav').addClass('opaque');
-//   }
-//   if (checkScroll() < 20) {
-//    $('nav').removeClass('opaque');
-//   }
-// }
-
 let navbar = $('.nav')
+let extended = false
+let height = $(window).scrollTop()
+changeNav(height)
 
 // Check Window Scroll Postion
 $(window).scroll(function() {
-    var height = $(window).scrollTop()
-    if (height > 10) {
-      navbar.addClass('short')
-    } else {
-      navbar.removeClass('short')
-    }
+  let height = $(window).scrollTop()
+  changeNav(height)
 })
 
-let extended = false
 function extendMenu() {
   if (extended) {
     document.querySelector(".nav").classList.remove("extended")
@@ -38,4 +16,12 @@ function extendMenu() {
     document.querySelector(".nav").classList.add("extended")
   }
   extended = !extended
+}
+
+function changeNav(height) {
+  if (height > 10) {
+    navbar.addClass('short')
+  } else {
+    navbar.removeClass('short')
+  }
 }
